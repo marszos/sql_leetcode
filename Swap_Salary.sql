08e8ac4a1985bd3818342a191a7950c23ac35c02
Marcin Szoska <93832363+marszos@users.noreply.github.com>
2022-12-30 12:53:21 -0500

    Create Swap_Salary.sql

---
+++ Swap_Salary.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+# Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) 
+# with a single update statement and no intermediate temporary tables.
+
+
+UPDATE Salary 
+SET sex = CASE
+            WHEN  sex ='f' THEN 'm'
+            ELSE 'f' 
+         END;
+
+
