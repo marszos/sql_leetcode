2552bd56a56bf57f9d4d9fb878ef8a6d0994c277
Marcin Szoska <93832363+marszos@users.noreply.github.com>
2023-01-20 20:49:04 -0500

    Update Rising_Temperature.sql

--- Rising_Temperature.sql
+++ Rising_Temperature.sql
@@ -4,3 +4,11 @@ from (
 select *, lag(temperature, 1) over (order by recordDate) as 'temp_yest'
 FROM Weather ) x 
 where x.temperature  > x.temp_yest
+
+
+
+select w2.id
+from Weather w1
+join Weather w2 
+on w2.recordDate - 1 = w1.recordDate
+where w2.temperature > w1.temperature
