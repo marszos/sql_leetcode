5926bce804693812525d92fc987499d6ef012fc6
Marcin Szoska <93832363+marszos@users.noreply.github.com>
2023-01-20 21:04:59 -0500

    Update Rising_Temperature.sql

--- Rising_Temperature.sql
+++ Rising_Temperature.sql
@@ -1,9 +1,20 @@
-# Write your MySQL query statement below
+# Write your MySQL query statement below // doesnt work on big dataset 
 select id 
 from (
 select *, lag(temperature, 1) over (order by recordDate) as 'temp_yest'
 FROM Weather ) x 
 where x.temperature  > x.temp_yest
+
+
+SELECT id
+FROM
+(
+SELECT id, recordDate, temperature AS today_temp, LAG(temperature,1) OVER (ORDER BY recordDate) AS yesterday_temp, LAG(recordDate,1) OVER (ORDER BY recordDate) AS yes_date
+
+FROM Weather
+) AS temp_table
+
+WHERE today_temp>yesterday_temp AND DATEDIFF(recordDate,yes_date)=1
 
 
 
